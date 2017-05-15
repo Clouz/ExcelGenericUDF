5fe2c875fbb4ce7e4018b90b6e8c917eb5af84c4
Clouz <clouz85@gmail.com>
2017-05-15 14:03:11 +0200

    Inizio Test NumeraFogli

---
+++ ClassiDiScambio/NumeraFogli/Sheet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassiDiScambio.NumeraFogli
+{
+    public class Sheet
+    {
+        public string Cell { get; set; }
+        public int FromSheet { get; set; }
+        public int ToSheet { get; set; }
+        public int TotalSheet { get; set; }
+        public int StartingNumber { get; set; }
+    }
+}
---
+++ ExcelGui/NumeraFogli.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using ClassiDiScambio.NumeraFogli;
+
+
+namespace ExcelGui
+{
+    /// <summary>
+    /// Logica di interazione per NumeraFogli.xaml
+    /// </summary>
+    public partial class NumeraFogli : Window
+    {
+        public NumeraFogli(Sheet data)
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void TextBlock_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
