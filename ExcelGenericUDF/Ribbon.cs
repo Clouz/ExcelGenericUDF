303f51aa333ccbd36acbdd1441e43a0ebe25b406
Clouz <clouz85@gmail.com>
2017-05-16 13:53:56 +0200

    Aggiunto Ribbon

--- ExcelGenericUDF/Ribbon.cs
+++ ExcelGenericUDF/Ribbon.cs
@@ -12,12 +12,34 @@ using ExcelDna.Integration.CustomUI;
 
 using System.Runtime.InteropServices;
 
+using System.Runtime.InteropServices;
+using ExcelDna.Integration.CustomUI;
+using System.Windows.Forms;
+
 namespace ExcelGenericUDF
 {
     [ComVisible(true)]
-    class Ribbon : ExcelRibbon
+    public class Ribbon : ExcelRibbon
     {
-        static public void prova()
+
+        public override string GetCustomUI(string RibbonID)
+        {
+            return @"
+<customUI xmlns='http://schemas.microsoft.com/office/2006/01/customui'>
+    <ribbon>
+    <tabs>
+        <tab id='tab1' label='My Tab'>
+        <group id='group1' label='My Group'>
+            <button id='button1' label='My Button' onAction='OnButtonPressed'/>
+        </group >
+        </tab>
+    </tabs>
+    </ribbon>
+</customUI>";
+        }
+
+
+        public void OnButtonPressed(IRibbonControl control)
         {
             var test = new ClassiDiScambio.NumeraFogli.Sheet()
             {
@@ -31,5 +53,7 @@ namespace ExcelGenericUDF
             var lala = new ExcelGui.NumeraFogli(test);
             lala.ShowDialog();
         }
+
+
     }
 }
